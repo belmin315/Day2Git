6c2c5b93f5eb507aa8f56951f254a38a486e01c6
Belmin <Belmin@DESKTOP-E8ECCRD>
2023-04-10 19:24:55 -0400

    Initial Commit

---
+++ src/day2/Day2Demo.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Day2Demo {
+	
+	public void getInfo() {
+		
+		System.out.println("Pushing code.....");
+		
+		System.out.println("Pushing code 2nd time....");
+		
+		System.out.println("Pushing code 3rd time .....");
+		
+	}
+
+}
